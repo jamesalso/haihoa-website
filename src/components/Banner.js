82209b90774b158d71fe8b4926f20bcd57fe597f
Leon James <minhkhuong160899@gmail.com>
2023-10-11 02:09:02 +0700

    Feat: Update Banner haihoa landing page

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,14 +4,14 @@ import React from 'react';
 export default function Banner() {
     return (
         <section className="bg-[#3255A8]" id="ingredient">
-            <div className="flex justify-center items-center h-[100vh] px-[350px]">
-                <div className="text-center italic text-[32px] text-[#ffffff] font-[SVN-Brice]">
-                    “<span className="text-[#ffdd04]">haihoa</span> là một chiến
-                    dịch truyền thông tăng nhận thức cho những người trẻ về hiện
-                    tượng `
+            <div className="flex justify-center items-center h-[90vh] px-[450px]">
+                <div className="text-center italic text-[35px] text-[#ffffff] font-[cormorant-garamond]">
+                    “ <span className="text-[#ffdd04]">haihoa</span> là một
+                    chiến dịch truyền thông tăng nhận thức cho những người trẻ
+                    về hiện tượng `
                     <span className="text-[#ee5b9f]">people pleaser</span>` -
                     những người thích làm hài lòng người khác và khuyến khích
-                    tinh thần tôn trọng bản thân.”
+                    tinh thần tôn trọng bản thân. ”
                 </div>
             </div>
         </section>
