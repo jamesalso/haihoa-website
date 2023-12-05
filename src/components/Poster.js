7fec227f477ba1c8b2305875c4ae3ff6e2821878
Leon James <minhkhuong160899@gmail.com>
2023-12-05 10:08:34 +0700

    Fix: Height poster event

--- src/components/Poster.js
+++ src/components/Poster.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default function Poster() {
     return (
         <section
-            className={`bg-[url(/images/poster/Poster-Haihoa.png)] bg-cover bg-center bg-no-repeat lg:py-[450px] sm:py-[60px]`}
+            className={`bg-[url(/images/poster/Poster-Haihoa.png)] bg-cover bg-center bg-no-repeat lg:py-[450px] sm:py-[150px]`}
             id="event"
         ></section>
     );
