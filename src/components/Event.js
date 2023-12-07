ed6106dbd4f3350d7ac515d837f4d23c89339e1c
Leon James <minhkhuong160899@gmail.com>
2023-12-07 17:42:51 +0700

    Fix: display Event page

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,6 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
 import { useEffect, useState } from 'react';
-import React from 'react';
 import Slider from 'react-slick';
 
 const NextArrow = (props) => {
@@ -104,46 +103,49 @@ export default function Event() {
             id="event"
         >
             <div className="lg:flex-col sm:flex sm:flex-col">
-                <div className="lg:mx-auto lg:mt-4 sm:mt-6 max-w-screen-xl lg:pl-[20px] lg:px-[35px] sm:px-4">
-                    <div className="p-4 border-[6px] border-[#ee5b9f] rounded-[35px] lg:!w-[900px] mx-auto">
-                        <div className="text-[#2954ad] text-center lg:text-[24px] sm:text-[20px] font-[SVN-Brice-SemiBold]">
-                            EVENT: HÀI HÒA LAND
-                        </div>
-                        <hr className="border-[#ee5b9f] lg:border-2 sm:border border-dashed mx-auto mt-2" />
-                        <div className="mt-3 text-[#ee5b9f] text-center lg:text-[17px] sm:text-[15px] font-[SVN-Brice-SemiBold]">
-                            SỰ KIỆN TRẢI NGHIỆM “HÀI HOÀ LAND” - CÙNG bé haihoa
-                            ĐẢO QUANH XỨ HÀI HOÀ
-                        </div>
-                        <div className="mx-2 text-[#2954ad] text-left">
-                            <div className="lg:text-[17px] sm:text-[15px] font-[SVN-Brice-SemiBold]">
-                                Chào mừng bạn đến với Hài Hòa Land!
+                <div className="lg:mx-auto lg:mt-4 sm:mt-6 max-w-screen-xl lg:pl-[20px] lg:px-[35px]">
+                    <Slider
+                        {...settings}
+                        className="lg:pl-[20px] sm:text-center"
+                    >
+                        <div className="lg:p-4 sm:p-2 border-[6px] border-[#ee5b9f] rounded-[35px] lg:!w-[900px] sm:!w-[400px]">
+                            <div className="text-[#2954ad] text-center lg:text-[24px] sm:text-[20px] font-[SVN-Brice-SemiBold]">
+                                EVENT: HÀI HÒA LAND
                             </div>
-                            <div className="font-[SVN-Brice] sm:text-[15px]">
-                                Chắc chắn bạn đã từng nghe về bé haihoa, người
-                                đến từ xứ sở Hài Hòa, nơi mà mọi người sống hài
-                                hòa và yêu thương. Chúng mình tự hào giới thiệu
-                                sự kiện trải nghiệm `Hài Hòa Land` - một chuỗi
-                                hoạt động tương tác và không gian trưng bày độc
-                                đáo, sẽ đưa bạn đến với cuộc phiêu lưu thú vị
-                                của bé haihoa và những bài học quý giá về việc
-                                được thể hiện được nhu cầu và mong muốn của bản
-                                thân nhưng vẫn duy trì được sự hài hòa, hòa
-                                thuận trong cuộc sống.
+                            <hr className="border-[#ee5b9f] lg:border-2 sm:border border-dashed mx-auto mt-2" />
+                            <div className="px-3 mt-3 text-[#ee5b9f] text-center lg:text-[18px] sm:text-[15px] font-[SVN-Brice-SemiBold]">
+                                SỰ KIỆN TRẢI NGHIỆM “HÀI HOÀ LAND” - CÙNG bé
+                                haihoa ĐẢO QUANH XỨ HÀI HOÀ
+                            </div>
+                            <div className="mx-2 text-[#2954ad] text-left">
+                                <div className="lg:text-[18px] sm:text-[15px] font-[SVN-Brice-SemiBold]">
+                                    Chào mừng bạn đến với Hài Hòa Land!
+                                </div>
+                                <div className="font-[SVN-Brice] text-[16px]">
+                                    Chắc chắn bạn đã từng nghe về bé haihoa,
+                                    người đến từ xứ sở Hài Hòa, nơi mà mọi người
+                                    sống hài hòa và yêu thương. Chúng mình tự
+                                    hào giới thiệu sự kiện trải nghiệm `Hài Hòa
+                                    Land` - một chuỗi hoạt động tương tác và
+                                    không gian trưng bày độc đáo, sẽ đưa bạn đến
+                                    với cuộc phiêu lưu thú vị của bé haihoa và
+                                    những bài học quý giá về việc được thể hiện
+                                    được nhu cầu và mong muốn của bản thân nhưng
+                                    vẫn duy trì được sự hài hòa, hòa thuận trong
+                                    cuộc sống.
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <hr className="border-[#ee5b9f] bg-[#ee5b9f] border-2 mx-auto w-[3px] h-[20px]" />
-                    <div className="flex lg:flex-row sm:flex-col mx-auto">
-                        <div className="px-2 py-4 border-[6px] border-[#ee5b9f] rounded-[35px] lg:!w-[500px]">
-                            <div className="text-[#2954ad] text-center text-[20px] font-[SVN-Brice-SemiBold] min-h-[67px]">
+                        <div className="px-2 py-4 border-[6px] border-[#ee5b9f] rounded-[35px] lg:!w-[550px] sm:!w-[400px] sm:min-h-[321px]">
+                            <div className="text-[#2954ad] text-center text-[22px] font-[SVN-Brice-SemiBold] min-h-[67px]">
                                 TRIỂN LÃM
                             </div>
-                            <hr className="border-[#ee5b9f] border-2 border-dashed mx-auto my-2" />
-                            <div className="mx-2 text-[#2954ad] text-[14px] text-left">
-                                <div className="font-[SVN-Brice-SemiBold]">
+                            <hr className="border-[#ee5b9f] lg:border-2 sm:border border-dashed mx-auto my-2" />
+                            <div className="sm:mt-4 mx-2 text-[#2954ad] text-left">
+                                <div className="font-[SVN-Brice-SemiBold] lg:text-[17px] sm:text-[15px]">
                                     Bộ Truyện Tranh `haihoa-pleaser` có 1-0-2
                                 </div>
-                                <div className="font-[SVN-Brice] text-[13px]">
+                                <div className="font-[SVN-Brice] text-[16px]">
                                     Dạo quanh và khám phá 8 câu chuyện đời thực
                                     về hành vi làm hài lòng người khác của bé
                                     haihoa-pleaser khi còn ở xứ sở Hài Hòa. Đây
@@ -153,15 +155,15 @@ export default function Event() {
                                 </div>
                             </div>
                         </div>
-                        <hr className="border-[#ee5b9f] bg-[#ee5b9f] border-2 mx-auto lg:w-[20px] sm:w-[3px] lg:h-[3px] sm:h-[18px] my-auto" />
-                        <div className="px-2 py-4 border-[6px] border-[#ee5b9f] rounded-[35px] lg:!w-[500px]">
-                            <div className="text-[#2954ad] text-center text-[20px] font-[SVN-Brice-SemiBold] min-h-[67px]">
+
+                        <div className="px-2 py-4 border-[6px] border-[#ee5b9f] rounded-[35px] lg:!w-[550px] sm:!w-[400px] sm:min-h-[321px]">
+                            <div className="text-[#2954ad] text-center text-[22px] font-[SVN-Brice-SemiBold] min-h-[67px]">
                                 HOẠT ĐỘNG TRẢI NGHIỆM
                                 <br /> F.A.S.T Skill
                             </div>
-                            <hr className="border-[#ee5b9f] border-2 border-dashed mx-auto my-2" />
-                            <div className="mx-2 text-[#2954ad] text-left">
-                                <div className="font-[SVN-Brice] text-[13px]">
+                            <hr className="border-[#ee5b9f] lg:border-2 sm:border border-dashed mx-auto my-2" />
+                            <div className="sm:mt-4 mx-2 text-[#2954ad] text-left">
+                                <div className="font-[SVN-Brice] text-[16px]">
                                     Tự tin, tôn trọng bản thân, và không sợ thể
                                     hiện ý kiến cá nhân! Trải nghiệm các hoạt
                                     động thiết kế dựa trên lý thuyết F.A.S.T để
@@ -173,17 +175,17 @@ export default function Event() {
                                 </div>
                             </div>
                         </div>
-                        <hr className="border-[#ee5b9f] bg-[#ee5b9f] border-2 mx-auto lg:w-[20px] sm:w-[3px] lg:h-[3px] sm:h-[18px] my-auto" />
-                        <div className="px-2 py-4 border-[6px] border-[#ee5b9f] rounded-[35px] lg:!w-[500px]">
-                            <div className="text-[#2954ad] text-center text-[20px] font-[SVN-Brice-SemiBold] min-h-[67px]">
+
+                        <div className="px-2 py-4 border-[6px] border-[#ee5b9f] rounded-[35px] lg:!w-[550px] sm:!w-[400px] sm:min-h-[321px]">
+                            <div className="text-[#2954ad] text-center text-[22px] font-[SVN-Brice-SemiBold] min-h-[67px]">
                                 WORKSHOP
                             </div>
-                            <hr className="border-[#ee5b9f] border-2 border-dashed mx-auto my-2" />
-                            <div className="mx-2 text-[#2954ad] text-[14px] text-left">
-                                <div className="font-[SVN-Brice-SemiBold]">
+                            <hr className="border-[#ee5b9f] lg:border-2 sm:border border-dashed mx-auto my-2" />
+                            <div className="sm:mt-4 mx-2 text-[#2954ad] text-left">
+                                <div className="font-[SVN-Brice-SemiBold] lg:text-[17px] sm:text-[16px]">
                                     Tô Màu `bé haihoa` trên Túi Tote
                                 </div>
-                                <div className="font-[SVN-Brice] text-[13px]">
+                                <div className="font-[SVN-Brice] text-[16px]">
                                     Chắc chắn bạn không muốn bỏ lỡ Magic Hour
                                     với workshop tô màu `Bé Haihoa` lên túi
                                     tote. Tự do sáng tạo và thể hiện bản thân,
@@ -191,6 +193,17 @@ export default function Event() {
                                 </div>
                             </div>
                         </div>
+                    </Slider>
+                </div>
+
+                <div className="mt-7 flex justify-center items-center gap-2">
+                    <img
+                        alt=""
+                        src="/images/slide/9165417_swipe_left_icon.svg"
+                        className="!w-[25px] lg:hidden sm:block"
+                    />
+                    <div className="text-[#ee5b9f] text-[14px] font-[SVN-Brice] lg:hidden sm:block">
+                        Swipe
                     </div>
                 </div>
 
